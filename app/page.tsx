6d2f10568db2d1bd31d076148b0f78fc3e3f01cb
Rounak Raj <rounakrajg401@gmail.com>
2024-06-07 00:44:05 +0530

    bento grid completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Hero from "@/components/Hero";
+import Grid from "@/components/Grid";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
          <Hero />
+         <Grid />
       </div>
     </main>
   );
