7fe288ba29c0ef8f762b60022a1f295cbabbf7ec
Rounak Raj <rounakrajg401@gmail.com>
2024-06-17 00:57:24 +0530

    All complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,29 @@
-import Image from "next/image";
+"use client";
+
+import { navItems } from "@/data";
+
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
+import Footer from "@/components/Footer";
+import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
+import RecentProjects from "@/components/RecentProjects";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
-export default function Home() {
+const Home = () => {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
       <div className="max-w-7xl w-full">
-         <Hero />
-         <Grid />
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+        <RecentProjects />
+        <Clients />
+        <Experience />
+        <Footer />
       </div>
     </main>
   );
-}
+};
+
+export default Home;
